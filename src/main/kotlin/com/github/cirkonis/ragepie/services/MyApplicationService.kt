4186811077671112090c9b054d1d54edbe6eaca7
GitHub Action <action@github.com>
2021-03-18 14:18:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/cirkonis/ragepie/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cirkonis/ragepie/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cirkonis.ragepie.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cirkonis.ragepie.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cirkonis/ragepie/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cirkonis/ragepie/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cirkonis.ragepie.services
 
+import com.github.cirkonis.ragepie.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
